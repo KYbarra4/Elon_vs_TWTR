8757665dc09366c9e5d7ce5fd9ca54227d8ce443
Kevin Ybarra <kevin.ybarra@gmail.com>
2022-12-25 16:53:53 -0600

    commit

--- schema.sql
+++ schema.sql
@@ -3,15 +3,13 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "twtr_stock" (
+CREATE TABLE "twtr_all" (
     "Date" DATE   NOT NULL,
-    "Open" VARCHAR(30)   NOT NULL,
-    "High" DECIMAN(7)   NOT NULL,
-    "Low" DECIMAN(7)   NOT NULL,
-    "Close" (7)   NOT NULL,
-    "Volume" Integer(9)   NOT NULL,
-    "Dividends" INTEGER(5)   NOT NULL,
-    "Splits" INTEGER(5)   NOT NULL,
+    "Open" DECIMAL(7)   NOT NULL,
+    "High" DECIMAL(7)   NOT NULL,
+    "Low" DECIMAL(7)   NOT NULL,
+    "Close" DECIMAL(7)   NOT NULL,
+    "Volume" INTEGER   NOT NULL,
     CONSTRAINT "pk_twtr_all" PRIMARY KEY (
         "Date"
      )
@@ -19,8 +17,7 @@ CREATE TABLE "twtr_stock" (
 
 CREATE TABLE "twtr_dates" (
     "Date" DATE   NOT NULL,
-    "News_Header" VARCHAR(100)   NOT NULL,
-    "Description" VARCHAR(1000)   NOT NULL
+    "Headline" VARCHAR(100)   NOT NULL
 );
 
 ALTER TABLE "twtr_dates" ADD CONSTRAINT "fk_twtr_dates_Date" FOREIGN KEY("Date")
